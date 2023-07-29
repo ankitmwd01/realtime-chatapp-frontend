5c09b944bc0e8d5f36470ca12a2d123f8e447cb2
Ankit Mewada <ankitmwd01@gmail.com>
2023-07-29 10:53:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import ChatPage from "./Pages/ChatPage";
+import dotenv from  'react-dotenv'
+import "./App.css"
 function App() {
+ console.log(process.env.REACT_APP_ENDPOINT);
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+       <Routes>
+    <Route path="/" element={<HomePage/>}></Route>
+    <Route path="/chats"  element={<ChatPage/>}></Route>
+    </Routes>
+   </div>
   );
 }
 
